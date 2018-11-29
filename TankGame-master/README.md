aca28643be1d0b573ce47e72d1a886e4a7315e25
LAPTOP-CCQN564F\sam11 <sam11jacklin@gmail.com>
2018-11-29 17:20:46 +0000

    Tank Game continued. AI tanks can locate the user tank and the crosshair placement is also recognised.

---
+++ TankGame-master/README.md
@@ -0,0 +1,5 @@
+# TankGame
+An open-world head-to-head tank fight with simple AI, terrain, and advanced control system in Unreal 4.
+## Progression
+* Initial commit
+* Update of README.md
---
+++ TankGame-master/TankGame/Source/TankGame/Public/TankPlayerController.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tank.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKGAME_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+private:
+	ATank* GetControlledTank() const;
+
+
+
+	//Start the tank moving the barrel so that the shot would hit where
+	//the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	//return an out parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& OutHitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+		float CrosshairYLocation = 0.3333;
+
+	UPROPERTY(EditAnywhere)
+		float LineTraceRange = 1000000;
+
+	bool GetLookDirection(FVector2D ScreenLocation, FVector& LookDirection) const;
+	bool GetLookVectorHitLocation(FVector LookDirection, FVector& Hitlocation) const;
+};
